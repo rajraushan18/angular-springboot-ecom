997ea9b37f769967e3e5cbf895e7b51d704b362c
rajraushan <rajraushan.018@gmail.com>
2023-11-24 14:41:50 +0530

    module folder update

--- src/app/Module/feature/feature.module.ts
+++ src/app/Module/feature/feature.module.ts
@@ -1,12 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FeatureComponent } from './components/feature.component';
+import { HomeComponent } from './components/home/home.component';
+import { MainCarouselComponent } from './components/home/main-carousel/main-carousel.component';
+import { ProductSliderComponent } from './components/home/product-slider/product-slider.component';
+import { HomeProductCardComponent } from './components/home/home-product-card/home-product-card.component';
+import { ProductsComponent } from './components/products/products.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FeatureComponent,
+    HomeComponent,
+    MainCarouselComponent,
+    ProductSliderComponent,
+    HomeProductCardComponent,
+    ProductsComponent 
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    FeatureComponent,
+    HomeComponent,
+    ProductsComponent
   ]
 })
 export class FeatureModule { }
--- src/app/Module/shared/shared.module.ts
+++ src/app/Module/shared/shared.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavContentComponent } from './components/navbar/nav-content/nav-content.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button'
+import {MatMenuModule} from '@angular/material/menu';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NavbarComponent,
+    FooterComponent,
+    NavContentComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule
+  ],
+  exports: [
+    NavbarComponent,
+    FooterComponent    
   ]
 })
 export class SharedModule { }
