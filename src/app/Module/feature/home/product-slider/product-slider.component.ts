99df38455aa448ba12231cc6ef373588f26ef29c
rajraushan <rajraushan.018@gmail.com>
2023-11-24 14:09:27 +0530

    first commit

---
+++ src/app/Module/feature/home/product-slider/product-slider.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-product-slider',
+  templateUrl: './product-slider.component.html',
+  styleUrls: ['./product-slider.component.scss']
+})
+export class ProductSliderComponent {
+  @Input() title:any
+  @Input() products:any[] | undefined;
+
+
+
+
+}
---
+++ src/app/Module/shared/navbar/navbar.component.ts
@@ -0,0 +1,51 @@
+import { Component, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent {
+
+  currentSection:any
+  isNavbarContentOpen:any
+
+
+  openNavbarContent(section:any){
+    this.isNavbarContentOpen = true
+    this.currentSection=section
+  }
+
+  closeNavbarContent(){
+    this.isNavbarContentOpen=false
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    const modalContainer = document.querySelector('.modal-container');
+    const openButtons = document.querySelectorAll('.open-button');
+
+    let clickedInsideButton = false;
+
+    openButtons.forEach((button: Element) => {
+      if (button.contains(event.target as Node)) {
+        clickedInsideButton = true;
+      }
+    });
+
+    if (modalContainer && !clickedInsideButton && this.isNavbarContentOpen) {
+      // console.log(
+      //   'container ---------------------- ',
+      //   this.isNavbarContentOpen
+      // );
+      this.closeNavbarContent();
+    }
+  }
+
+  navigateTo(path:any){
+
+  }
+
+
+
+}
